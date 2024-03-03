5c0e0a85c6b279defa3fcd3b685c76230519c6c8
Anirudh <anirudh.uppal@gmail.com>
2024-03-03 19:18:24 +0530

    Code Commit-4

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -1,4 +1,10 @@
 import sys
 
 def helloworld(out):
+    """
+    Writes "Hello World of Python" to the given output stream.
+
+    Args:
+        out: Output stream to write the message to.
+    """
     out.write("Hello World of Python\n")
--- src/unittest/python/helloworld_tests.py
+++ src/unittest/python/helloworld_tests.py
@@ -1,3 +1,7 @@
+"""
+Module helloworld.py: Contains function to print "Hello world of Python".
+"""
+
 from mockito import mock, verify
 import unittest
 
@@ -5,10 +9,16 @@ from helloworld import helloworld
 
 
 class HelloWorldTest(unittest.TestCase):
+    """
+    Test case for the helloworld function.
+    """
+
     def test_should_issue_hello_world_message(self):
+        """
+        Test if helloworld function writes "Hello world of Python" to the output stream.
+        """
         out = mock()
 
         helloworld(out)
 
         verify(out).write("Hello world of Python\n")
-
